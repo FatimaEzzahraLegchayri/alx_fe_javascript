42f593e8aedfae40c9651ae1ec66c8e5408c5bc8
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-16 23:42:53 +0200

    fix displayRandomQuote function

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -6,7 +6,7 @@ const newQuoteCategory = document.getElementById('newQuoteCategory')
 let arr = JSON.parse(localStorage.getItem('QUOTE')) || [] 
 function showRandomQuote(){
     const randomQuote = arr[Math.floor(Math.random()*arr.length)]
-    quoteDisplay.textContent = randomQuote.text + ' - ' +  randomQuote.category   
+    quoteDisplay.innerHTML = randomQuote.text + ' - ' +  randomQuote.category   
 }
 showRandomQuote()
 newQuote.addEventListener('click',showRandomQuote)
