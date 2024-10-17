cdf3580abad36eec3dbf9c1521ad71cda3d1d9b9
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-17 16:24:32 +0200

    function to download json file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,8 +1,10 @@
+// const { type } = require("express/lib/response")
+
 const newQuote = document.getElementById('newQuote')
 const quoteDisplay = document.getElementById('quoteDisplay')
 const newQuoteText = document.getElementById('newQuoteText')
 const newQuoteCategory = document.getElementById('newQuoteCategory')
-
+const exportBtn = document.getElementById('exportBtn')
 
 let arr = JSON.parse(localStorage.getItem('QUOTE')) || [] 
 function showRandomQuote(){
@@ -13,21 +15,38 @@ showRandomQuote()
 newQuote.addEventListener('click',showRandomQuote)
 
 function addQuote(){
-    // console.log(newQuoteText.value,'llk');
     if(newQuoteText.value.length == 0 || newQuoteCategory.value.length==0){
         alert('all fiels are required.')
     }else{
         arr.push({text : newQuoteText.value, category : newQuoteCategory.value})
         localStorage.setItem('QUOTE',JSON.stringify(arr))
-        // console.log(arr,'arr');
-        newQuoteText.value.innerHTML = ''
-        newQuoteCategory.value.innerHTML = ''
+        // newQuoteText.value.innerHTML = ''
+        // newQuoteCategory.value.innerHTML = ''
+        newQuoteText.value = ''
+        newQuoteCategory.value = ''
     }
-
 }
-function createAddQuoteForm(){
 
+function exportQuotesToJSON(){
+    
+    const a = document.createElement('a')
+    // exportBtn.appendChild(a)
+    const arrTostring = JSON.stringify(arr);
+    const blob = new Blob([arrTostring], {type : 'text/plain'})
+    const url = URL.createObjectURL(blob)
+    console.log('blob',url);
+    a.href = url
+    a.download = 'quotes.json'
+    a.click()
 }
-const li = document.createElement('li')
-li.appendChild(ok)
+exportBtn.addEventListener("click", exportQuotesToJSON);
+
+// ///
+// function createAddQuoteForm(){
+
+// }
+// const li = document.createElement('li')
+// li.appendChild(ok)
 // document.addEventListener('click',document.getElementById())
+
+
