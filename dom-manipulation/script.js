014fda81c79284fba4edaf7d4ab1cf2d618963ae
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-23 03:03:49 +0200

    function to remove li based on user selection

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -5,31 +5,32 @@ const newQuoteCategory = document.getElementById('newQuoteCategory')
 const message = document.getElementById('message')
 const addQuote = document.getElementById('addQuote')
 const ul = document.getElementById('quotes')
+// const removeBtn = document.createElement('button')
 
 
-let arr = JSON.parse(localStorage.getItem('quoteList'))||[]
+let arr = JSON.parse(localStorage.getItem('quoteList')) || [];
 
 function generateQuote(){
     let randomNum = arr[Math.floor(Math.random() * arr.length)]
     
-    arr.lenght<0 ? 
-    quoteDisplay.textContent = 'no quote found!'
+    arr.length == 0 ? 
+    (
+        newQuote.style.display = 'none'
+    )
     :
-    quoteDisplay.textContent = `" ` + randomNum.text + ` "` + ' - ' + randomNum.ctg
+    (quoteDisplay.textContent = `" ` + randomNum.text + ` "` + ' - ' + randomNum.ctg)
 }
 newQuote.addEventListener('click',generateQuote)
 generateQuote()
 
 function addQuotes(e){
    e.preventDefault()
-   ul.innerHTML = ''
-    console.log('mmmmmmmmmmmmmmmmmmmmm',newQuoteText.value);
-    console.log('mmmmmmmmmmmmmmmmmmmmm',newQuoteCategory.value);
     
     if(newQuoteText.value == '' || newQuoteCategory.value == ''){
         message.textContent = 'all fields are required'
         message.style.color = 'red'
     }else{
+        ul.innerHTML = ''
         arr.push({text : newQuoteText.value, ctg : newQuoteCategory.value})
         message.textContent = ''
         localStorage.setItem('quoteList',JSON.stringify(arr))
@@ -38,6 +39,7 @@ function addQuotes(e){
         message.style.color = 'green'
         console.log('arr', arr)
         display()
+        generateQuote()
     }
 }
 newQuoteText.addEventListener('keypress',function(event){
@@ -54,14 +56,38 @@ newQuoteCategory.addEventListener('keypress',function(event){
 })
 
 function display(){
-    arr.forEach((e)=>{
-        // console.log('e', e.ctg)
-        const li = document.createElement('li')
-        li.textContent = `" ` + e.text + ` "` + ' - '+ e.ctg
-        ul.appendChild(li)
-    })
+    arr.length == 0 ?
+    (ul.textContent = 'No quote yet. add one!')
+    :
+    (
+        arr.forEach((e,i)=>{
+            const li = document.createElement('li')
+            const removeBtn = document.createElement('button')
+            removeBtn.textContent = 'remove'
+            removeBtn.id = 'removeBtn'
+            li.textContent = `" ` + e.text + ` "` + ' - '+ e.ctg
+            ul.appendChild(li)
+            li.appendChild(removeBtn)
+            removeBtn.addEventListener('click', function(){
+                remove(i,li)
+                generateQuote()   
+                
+
+            })
+            
+        })
+    )
 }
 display()
  
+function remove(i,li){
+    ul.removeChild(li)
+    arr.splice(i,1)
+    localStorage.setItem('quoteList',JSON.stringify(arr)) 
+}
+// removeBtn.addEventListener('click',remove)
 
-
+// document.getElementById('removeBtn').addEventListener('click',function remove(e,li){
+//     console.log('llllllllllll');
+// li.removeChild(e)
+// })
